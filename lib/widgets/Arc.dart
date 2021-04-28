e74ea4e1b3dca8cd6b022ae9cc4f85e86e717f9d
Dieter Nuytemans <dieter.nuytemans@student.uantwerpen.be>
2021-04-28 16:56:49 +0200

    Fix blue bottom line

--- lib/widgets/Arc.dart
+++ lib/widgets/Arc.dart
@@ -4,23 +4,29 @@ import 'dart:math' as math;
 class Arc extends StatelessWidget {
   final double diameter;
   final double border;
+  final Color backgroundColor;
 
-  const Arc({Key? key, this.diameter = 300, this.border = 50.0}) : super(key: key);
+  const Arc(
+      {Key? key,
+      this.diameter = 300,
+      this.border = 50.0,
+      this.backgroundColor = Colors.white})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return CustomPaint(
-      painter: ArcPainter(border),
+      painter: ArcPainter(border, backgroundColor),
       size: Size(diameter, diameter / 2),
     );
   }
 }
 
 class ArcPainter extends CustomPainter {
-
   final double border;
+  final Color backgroundColor;
 
-  ArcPainter(this.border);
+  ArcPainter(this.border, this.backgroundColor);
 
   @override
   void paint(Canvas canvas, Size size) {
@@ -44,12 +50,11 @@ class ArcPainter extends CustomPainter {
 
     // Cut out part of semi-circle
     Paint eraser = Paint();
-    eraser.blendMode = BlendMode.clear;
-    eraser.color = Colors.white;
+    eraser.color = backgroundColor;
     canvas.drawArc(
       Rect.fromCenter(
-        center: centerOffset,
-        height: (2 * size.height) - border,
+        center: Offset( centerOffset.dx, centerOffset.dy + 1 ),
+        height: (2 * size.height) - border - 1,
         width: size.width - border,
       ),
       math.pi,
